5ffd94ee1563a5ceb291c1e54ba9dd3b10363d45
mallick-portfolio <tamal@gmail.com>
2022-04-08 14:07:20 +0600

    Added hide show functionality in menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,35 @@
-import React from "react";
-import logo from '../../images/luxurycar.png'
+import { faHomeAlt, faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import logo from "../../images/luxurycar.png";
 const Navbar = () => {
+  const [open, setOpen] = useState(true);
   return (
-    <div className="bg-slate-700 sm:px-6">
+    <div className="bg-slate-700 sm:px-6 sticky top-0">
+      <div onClick={()=>setOpen(!open)} className={`sm:hidden pl-5 pt-5`}>
+        { open ? <FontAwesomeIcon icon={faX} /> : <FontAwesomeIcon icon={faHomeAlt} />}
+      </div>
       <div className="sm:flex justify-between items-center">
-        <div className="logo">
-         <img className="w-16 sm:25 h-auto" src={logo} alt="" />
+        <div className="hidden sm:block">
+          <img className="w-16 sm:25 h-auto" src={logo} alt="" />
         </div>
-        <nav className="mainMenu">
-          <ul className="sm:flex justify-end items-center">
-            <li className="uppercase sm:ml-5 text-white hover:ring-1 ring-offset-1 px-5 py-1 rounded-lg"><a href="/">Home</a></li>
-            <li className="uppercase sm:ml-5 text-white hover:ring-1 ring-offset-1 px-5 py-1 rounded-lg"><a href="/">Reviews</a></li>
-            <li className="uppercase sm:ml-5 text-white hover:ring-1 ring-offset-1 px-5 py-1 rounded-lg"><a href="/">Dashboard</a></li>
-            <li className="uppercase sm:ml-5 text-white hover:ring-1 ring-offset-1 px-5 py-1 rounded-lg"><a href="/">Blogs</a></li>
-            <li className="uppercase sm:ml-5 text-white hover:ring-1 ring-offset-1 px-5 py-1 rounded-lg"><a href="/">About</a></li>
+        <nav className={`duration-1000 ${open ? 'block' : 'hidden'}`}>
+          <ul className={`sm:flex justify-end items-center`}>
+            <li className="navItem">
+              <a href="/" className="inline-block">Home</a>
+            </li>
+            <li className="navItem">
+              <a href="/" className="inline-block">Reviews</a>
+            </li>
+            <li className="navItem">
+              <a href="/" className="inline-block">Dashboard</a>
+            </li>
+            <li className="navItem">
+              <a href="/" className="inline-block">Blogs</a>
+            </li>
+            <li className="navItem">
+              <a href="/" className="inline-block">About</a>
+            </li>
           </ul>
         </nav>
       </div>
